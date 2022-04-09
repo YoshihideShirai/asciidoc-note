01dfcc8c063467906462dcd9f23d2085b4fb2cc5
Yoshihide Shirai <yoshihide.shirai@gmail.com>
2022-04-09 12:35:24 +0900

    check directory

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,12 +13,14 @@ fi
 apt-get update
 apt-get install -y sudo git
 
-mkdir /npm
-cd /npm
-git clone -b bugfix-lunr-language-ja-doesnt-work https://gitlab.com/yoshihide.shirai/antora-lunr-extension.git
-cd antora-lunr-extension
-npm i
-cd $WORKDIR
+if [ ! -d /npm/antora-lunr-extension ]; then
+    mkdir /npm
+    cd /npm
+    git clone -b bugfix-lunr-language-ja-doesnt-work https://gitlab.com/yoshihide.shirai/antora-lunr-extension.git
+    cd antora-lunr-extension
+    npm i
+    cd $WORKDIR
+fi
 
 npm i -g @antora/cli @antora/site-generator @asciidoctor/core asciidoctor-kroki /npm/antora-lunr-extension/
 # @antora/lunr-extension
